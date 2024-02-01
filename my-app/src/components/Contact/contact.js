61aed8b325c87078ca4d84a20cb8d257f5a4e171
Irfan-Arshad <ify10_arshad@hotmail.co.uk>
2024-02-01 02:25:28 +0000

    Contact me form - Functional
    Added functionality to contact me form using EmailJS. Also added border animation for form submit button along with an alert. Form also clears upon submission.

--- my-app/src/components/Contact/contact.js
+++ my-app/src/components/Contact/contact.js
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './contact.css';
 import LinkedInIcon from '../../assets/LinkedInIcon.png';
+import emailjs from '@emailjs/browser';
 
-const contact = () => {
+const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_ck4gi4h', 'template_nk9u7gb', form.current, 'Hgiw3sO_8FBIjr01O')
+          .then((result) => {
+              console.log(result.text);
+              e.target.reset();
+              alert('Email Sent');
+          }, (error) => {
+              console.log(error.text);
+          });
+      };
+
   return (
+
         <section id="contactPage">
             <div id="contact">
                 <h1 className="contactPageTitle">Contact Me</h1>
                 <span className='contactDesc'>Please fill out the form below to get in touch with me!</span>
-                <form className='contactForm'>
-                    <input type='text' className='name' placeholder='Your Name'/>
-                    <input type='email' className='email' placeholder='Your Email'/>
+                <form className='contactForm' ref={form} onSubmit={sendEmail}>
+                    <input type='text' className='name' placeholder='Your Name' name="your_name" />
+                    <input type='email' className='email' placeholder='Your Email' name="your_email" S/>
                     <textarea type='text' className='message' name='message' rows='5' placeholder='Your Message'/>
                     <button type='submit' value='send' className='submitBtn'>Submit</button>
                     <div className='links'>
@@ -19,8 +36,9 @@ const contact = () => {
                 </form>
             </div>
         </section>
+
   );
 }
 
-export default contact;
+export default Contact;
 
