0e50f63ba09d481293f795dc1e15722b46afe05e
Irfan-Arshad <ify10_arshad@hotmail.co.uk>
2024-02-18 03:01:38 +0000

    Added Typewriter effect
    Changed static text of 'Graduate Developer' to dynamic typewriter effect text that has animation.

--- my-app/src/components/Intro/intro.js
+++ my-app/src/components/Intro/intro.js
@@ -3,15 +3,23 @@ import './intro.css';
 import bg from '../../assets/Me.png';
 import btnImg from '../../assets/hireme.png';
 import cv from '../../assets/Irfan Arshad - Developer - CV - 2024.pdf'
+import { useTypewriter, Cursor } from 'react-simple-typewriter';
+// import { useTypewriter } from 'react-simple-typewriter';
 // import { Link } from 'react-scroll';
 
 const Intro = () => {
+  const [typeEffect] = useTypewriter({
+    words: ['Software Developer', 'Web Developer', 'Game Developer'],
+    loop:{},
+    typeSpeed: 100,
+    deleteSpeed: 40
+  })
   return (
     <section id="intro">
       <div className="introContent">
         <span className="hello">Hello World, </span>
-        <span className="introText">I'm <span className="introName">Irfan</span> <br />Graduate Developer</span>
-        <p className="introPara"> I'm an enthusiastic recent graduate with a strong educational foundation <br /> and a passion for launching my career in the field of software / web development.</p>
+        <span className="introText">I'm <span className="introName">Irfan</span> <br />{typeEffect}<Cursor/> </span>
+        <p className="introPara"> I'm an enthusiastic recent graduate with a strong educational foundation <br /> and a passion for launching my career in the field technology.</p>
         <div>
           <button className='btn' onClick={() => {
             document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
